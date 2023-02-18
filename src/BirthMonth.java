8d1808f5f29811823c1879db66513bd32baa8c44
Lucas <gibsonlc@mail.uc.edu>
2023-02-18 11:01:08 -0500

    Project init

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,12 +1,17 @@
 public class BirthMonth {
     public static void main(String[] args) {
+        //Declare Variables
         int birthMonth = 0;
+        //Prompt
         System.out.println("What is your birth month?");
+        //Input
         System.out.println(birthMonth);
+        //Output If Correct
         if(birthMonth <= 12 && birthMonth > 0)
         {
             System.out.println("Your birth month is " + birthMonth);
         }
+        //Output If Incorrect
         else
         {
             System.out.println("You entered an incorrect month value: " + birthMonth);
